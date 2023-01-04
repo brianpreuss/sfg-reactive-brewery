76c3a9bee689c050c5b352be4896eabf91909e94
Brian Preuß <brian.preuss@googlemail.com>
2023-01-04 16:36:28 +0100

    47 - Maven Dependencies

--- src/main/java/guru/springframework/sfgrestbrewery/domain/Beer.java
+++ src/main/java/guru/springframework/sfgrestbrewery/domain/Beer.java
@@ -4,16 +4,7 @@ import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.UUID;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import guru.springframework.sfgrestbrewery.web.model.BeerStyleEnum;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Version;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,15 +17,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Entity
+// @Entity
 public class Beer {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.UUID)
-  @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+  // @Id
+  // @GeneratedValue(strategy = GenerationType.UUID)
+  // @Column(length = 36, columnDefinition = "varchar", updatable = false,
+  // nullable = false)
   private UUID id;
 
-  @Version
+  // @Version
   private Long version;
 
   private String beerName;
@@ -44,10 +36,10 @@ public class Beer {
   private Integer quantityOnHand;
   private BigDecimal price;
 
-  @CreationTimestamp
-  @Column(updatable = false)
+  // @CreationTimestamp
+  // @Column(updatable = false)
   private Timestamp createdDate;
 
-  @UpdateTimestamp
+  // @UpdateTimestamp
   private Timestamp lastModifiedDate;
 }
--- src/main/java/guru/springframework/sfgrestbrewery/services/BeerServiceImpl.java
+++ src/main/java/guru/springframework/sfgrestbrewery/services/BeerServiceImpl.java
@@ -1,5 +1,14 @@
 package guru.springframework.sfgrestbrewery.services;
 
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
 import guru.springframework.sfgrestbrewery.domain.Beer;
 import guru.springframework.sfgrestbrewery.repositories.BeerRepository;
 import guru.springframework.sfgrestbrewery.web.controller.NotFoundException;
@@ -9,14 +18,6 @@ import guru.springframework.sfgrestbrewery.web.model.BeerPagedList;
 import guru.springframework.sfgrestbrewery.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 /**
  * Created by jt on 2019-04-20.
@@ -25,93 +26,84 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class BeerServiceImpl implements BeerService {
-    private final BeerRepository beerRepository;
-    private final BeerMapper beerMapper;
+  private final BeerRepository beerRepository;
+  private final BeerMapper beerMapper;
 
-    @Cacheable(cacheNames = "beerListCache", condition = "#showInventoryOnHand == false ")
-    @Override
-    public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand) {
+  @Cacheable(cacheNames = "beerListCache", condition = "#showInventoryOnHand == false ")
+  @Override
+  public BeerPagedList listBeers(
+      final String beerName,
+      final BeerStyleEnum beerStyle,
+      final PageRequest pageRequest,
+      final Boolean showInventoryOnHand) {
 
-        BeerPagedList beerPagedList;
-        Page<Beer> beerPage;
+    BeerPagedList beerPagedList;
+    Page<Beer> beerPage;
 
-        if (!StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
-            //search both
-            beerPage = beerRepository.findAllByBeerNameAndBeerStyle(beerName, beerStyle, pageRequest);
-        } else if (!StringUtils.isEmpty(beerName) && StringUtils.isEmpty(beerStyle)) {
-            //search beer_service name
-            beerPage = beerRepository.findAllByBeerName(beerName, pageRequest);
-        } else if (StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
-            //search beer_service style
-            beerPage = beerRepository.findAllByBeerStyle(beerStyle, pageRequest);
-        } else {
-            beerPage = beerRepository.findAll(pageRequest);
-        }
-
-        if (showInventoryOnHand){
-            beerPagedList = new BeerPagedList(beerPage
-                    .getContent()
-                    .stream()
-                    .map(beerMapper::beerToBeerDtoWithInventory)
-                    .collect(Collectors.toList()),
-                    PageRequest
-                            .of(beerPage.getPageable().getPageNumber(),
-                                    beerPage.getPageable().getPageSize()),
-                    beerPage.getTotalElements());
-        } else {
-            beerPagedList = new BeerPagedList(beerPage
-                    .getContent()
-                    .stream()
-                    .map(beerMapper::beerToBeerDto)
-                    .collect(Collectors.toList()),
-                    PageRequest
-                            .of(beerPage.getPageable().getPageNumber(),
-                                    beerPage.getPageable().getPageSize()),
-                    beerPage.getTotalElements());
-        }
-
-        return beerPagedList;
+    if (!StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
+      // search both
+      beerPage = beerRepository.findAllByBeerNameAndBeerStyle(beerName, beerStyle, pageRequest);
+    } else if (!StringUtils.isEmpty(beerName) && StringUtils.isEmpty(beerStyle)) {
+      // search beer_service name
+      beerPage = beerRepository.findAllByBeerName(beerName, pageRequest);
+    } else if (StringUtils.isEmpty(beerName) && !StringUtils.isEmpty(beerStyle)) {
+      // search beer_service style
+      beerPage = beerRepository.findAllByBeerStyle(beerStyle, pageRequest);
+    } else {
+      beerPage = beerRepository.findAll(pageRequest);
     }
 
-    @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false ")
-    @Override
-    public BeerDto getById(UUID beerId, Boolean showInventoryOnHand) {
-        if (showInventoryOnHand) {
-            return beerMapper.beerToBeerDtoWithInventory(
-                    beerRepository.findById(beerId).orElseThrow(NotFoundException::new)
-            );
-        } else {
-            return beerMapper.beerToBeerDto(
-                    beerRepository.findById(beerId).orElseThrow(NotFoundException::new)
-            );
-        }
+    if (showInventoryOnHand) {
+      beerPagedList = new BeerPagedList(
+        beerPage.getContent().stream().map(beerMapper::beerToBeerDtoWithInventory).collect(Collectors.toList()),
+        PageRequest.of(beerPage.getPageable().getPageNumber(), beerPage.getPageable().getPageSize()),
+        beerPage.getTotalElements()
+      );
+    } else {
+      beerPagedList = new BeerPagedList(
+        beerPage.getContent().stream().map(beerMapper::beerToBeerDto).collect(Collectors.toList()),
+        PageRequest.of(beerPage.getPageable().getPageNumber(), beerPage.getPageable().getPageSize()),
+        beerPage.getTotalElements()
+      );
     }
 
-    @Override
-    public BeerDto saveNewBeer(BeerDto beerDto) {
-        return beerMapper.beerToBeerDto(beerRepository.save(beerMapper.beerDtoToBeer(beerDto)));
+    return beerPagedList;
+  }
+
+  @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false ")
+  @Override
+  public BeerDto getById(final UUID beerId, final Boolean showInventoryOnHand) {
+    if (showInventoryOnHand) {
+      return beerMapper.beerToBeerDtoWithInventory(beerRepository.findById(beerId).orElseThrow(NotFoundException::new));
     }
+    return beerMapper.beerToBeerDto(beerRepository.findById(beerId).orElseThrow(NotFoundException::new));
+  }
 
-    @Override
-    public BeerDto updateBeer(UUID beerId, BeerDto beerDto) {
-        Beer beer = beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
+  @Override
+  public BeerDto saveNewBeer(final BeerDto beerDto) {
+    return beerMapper.beerToBeerDto(beerRepository.save(beerMapper.beerDtoToBeer(beerDto)));
+  }
 
-        beer.setBeerName(beerDto.getBeerName());
-        beer.setBeerStyle(BeerStyleEnum.PILSNER.valueOf(beerDto.getBeerStyle()));
-        beer.setPrice(beerDto.getPrice());
-        beer.setUpc(beerDto.getUpc());
+  @Override
+  public BeerDto updateBeer(final UUID beerId, final BeerDto beerDto) {
+    final Beer beer = beerRepository.findById(beerId).orElseThrow(NotFoundException::new);
 
-        return beerMapper.beerToBeerDto(beerRepository.save(beer));
-    }
+    beer.setBeerName(beerDto.getBeerName());
+    beer.setBeerStyle(BeerStyleEnum.valueOf(beerDto.getBeerStyle()));
+    beer.setPrice(beerDto.getPrice());
+    beer.setUpc(beerDto.getUpc());
 
-    @Cacheable(cacheNames = "beerUpcCache")
-    @Override
-    public BeerDto getByUpc(String upc) {
-        return beerMapper.beerToBeerDto(beerRepository.findByUpc(upc));
-    }
+    return beerMapper.beerToBeerDto(beerRepository.save(beer));
+  }
 
-    @Override
-    public void deleteBeerById(UUID beerId) {
-        beerRepository.deleteById(beerId);
-    }
+  @Cacheable(cacheNames = "beerUpcCache")
+  @Override
+  public BeerDto getByUpc(final String upc) {
+    return beerMapper.beerToBeerDto(beerRepository.findByUpc(upc));
+  }
+
+  @Override
+  public void deleteBeerById(final UUID beerId) {
+    beerRepository.deleteById(beerId);
+  }
 }
